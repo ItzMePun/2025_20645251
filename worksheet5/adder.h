352a9b6579a4b1b9f0cd5aba36c2a2d25e720b96
Pun <pun.phana@gmail.com>
2026-02-12 13:19:13 +0000

    worksheet 5 start

---
+++ worksheet5/adder.h
@@ -0,0 +1,17 @@
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+
+/**
+@file
+this file contains all declarations of exported functions in the maths library
+*/
+
+/**	
+	Adding function								[Brief Description]
+	This function adds two parameter numbers	[More detail]
+	@param a is the first number				[parameter definition]
+	@param b is the second number				[parameter definition]
+	@return sum of a and b
+*/
+int add(int a, int b);
+#endif // !MATHSLIB_ADDER_H
